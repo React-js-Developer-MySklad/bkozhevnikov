0c308a0199b10c94fac2028e0537b899abd890b7
bkozhevnikov <bkozhevnikov@moysklad.ru>
2024-08-29 22:29:33 +0300

    review from PR

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,17 @@
 import './style.css'
-import {createRoot} from "react-dom/client";
-import {App} from "./components/app/app";
-import {StrictMode} from "react";
-import { CounterpartyApiContext, CounterpartyApiImpl } from "./components/hooks/CounterpartyApiContext";
+import { createRoot } from "react-dom/client";
+import { App } from "./components/app/app";
+import { StrictMode } from "react";
+import { CounterpartyApiProvider } from './components/hooks/CounterpartyApiProvider';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement)
 
 root.render(
     <StrictMode>
-        <CounterpartyApiContext.Provider value={CounterpartyApiImpl}>
-            <App/>
-        </CounterpartyApiContext.Provider>
+        <CounterpartyApiProvider>
+            <App />
+        </CounterpartyApiProvider>
+
     </StrictMode>
 );
