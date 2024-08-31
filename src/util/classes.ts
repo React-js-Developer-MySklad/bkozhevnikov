5bc2638f836d0890b37f3a7d6775a3498f35275f
bkozhevnikov <bkozhevnikov@moysklad.ru>
2024-08-31 21:42:00 +0300

    hm5 - форма + валидация через конфигурацию

--- src/util/classes.ts
+++ src/util/classes.ts
@@ -7,11 +7,25 @@ export type Counterparty = {
     kpp?: string;
 }
 
+type hasMessage = {
+    message?: string;
+}
+
+type ValidationResult = {
+    name?: hasMessage;
+    inn?: hasMessage;
+    address?: hasMessage;
+    kpp?: hasMessage;
+}
+
 export type TableConfiguration = {
     columnNames: string[];
     valueGetter: (columnName: string, value: Counterparty) => string;
     valueSetter: (columnName: string, c: Counterparty, value: string) => Counterparty;
+    validation?: (counterparty: Counterparty) => ValidationResult | undefined;
 }
+
+const regexForLiteralAndNumber = /^[A-Za-z0-9]+$/;
 
 export const defaultConfiguration: TableConfiguration = {
     columnNames: ['name', 'inn', 'address', 'kpp'],
@@ -33,14 +47,52 @@ export const defaultConfiguration: TableConfiguration = {
     valueSetter: (columnName: string, counterparty: Counterparty, value) => {
         switch (columnName) {
             case 'name':
-                return {...counterparty, 'name': value}
+                return { ...counterparty, 'name': value }
             case 'inn':
-                return {...counterparty, 'inn': value}
+                return { ...counterparty, 'inn': value }
             case 'address':
-                return {...counterparty, 'address': value}
+                return { ...counterparty, 'address': value }
             case 'kpp':
-                return {...counterparty, 'kpp': value}
+                return { ...counterparty, 'kpp': value }
         }
-        return {...counterparty};
+        return { ...counterparty };
+    },
+
+    validation: (c: Counterparty) => {
+        let hasError: boolean = false;
+        let validationResult: ValidationResult = {}
+        if (!c.name || c.name.trim().length === 0) {
+            validationResult = { ...validationResult, name: { message: "Поле должно быть заполнено" } }
+            hasError = true;
+        }
+
+        if (c.inn) {
+            if (!regexForLiteralAndNumber.test(c.inn)) {
+                validationResult = { ...validationResult, inn: { message: "Поле может содердать только цифры и латинские буквы " } }
+                hasError = true;
+            } else if (c.inn.length != 11) {
+                validationResult = { ...validationResult, inn: { message: "Поле должно содержать 11 символов " } }
+                hasError = true;
+            }
+        } else {
+            validationResult = { ...validationResult, inn: { message: "Поле должно быть заполнено" } }
+            hasError = true;
+        }
+        if (c.kpp) {
+            if (!regexForLiteralAndNumber.test(c.kpp)) {
+                validationResult = { ...validationResult, kpp: { message: "Поле может содердать только цифры и латинские буквы " } }
+                hasError = true;
+            } else if (c.kpp.length != 9) {
+                validationResult = { ...validationResult, kpp: { message: "Поле должно содержать 9 символов " } }
+                hasError = true;
+            }
+        } else {
+            validationResult = { ...validationResult, kpp: { message: "Поле должно быть заполнено" } }
+            hasError = true;
+        }
+        if (hasError) {
+            return validationResult;
+        }
+        return undefined;
     }
 }
